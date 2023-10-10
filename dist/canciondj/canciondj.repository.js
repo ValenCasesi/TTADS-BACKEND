84ca3a0a2a40a452226f8f4f0dfc05da4456f609
valentino <valecasesi@gmail.com>
2023-10-10 17:00:54 -0300

    add .gitignore

--- dist/canciondj/canciondj.repository.js
+++ dist/canciondj/canciondj.repository.js
@@ -1,13 +1,21 @@
 import { db } from '../shared/db/conn.js';
 // pedimos la coleccion de tlos djs en la db y casteamos <Dj> para q traiga una coleccion con objetos Dj de entity.
-const djs = db.collection('canciondj');
+const canciondjs = db.collection('canciondj');
 export class CanciondjRepository {
     // Promise<Dj[] | undefined> --> eso indica que la funcion devuelve una promesa con array de djs o undefined si no encuentra nada.
     async findAll() {
-        return await djs.find().toArray(); //lo convierte a array ya que el find devuelve un cursor
+        return await canciondjs.find().toArray(); //lo convierte a array ya que el find devuelve un cursor
     }
     async add(item) {
-        item._id = (await djs.insertOne(item)).insertedId; //aca buscamos el id del item que se inserta para luego poder mostrarlo. insertedId recupera el id del item
+        const dj = item.dj;
+        const cancion = item.cancion;
+        const fechaNoche = item.fechaNoche;
+        const puntaje = item.puntaje;
+        const cancionDjExistente = await canciondjs.findOne({ dj, cancion, fechaNoche, puntaje });
+        if (cancionDjExistente) {
+            return undefined;
+        }
+        item._id = (await canciondjs.insertOne(item)).insertedId; //aca buscamos el id del item que se inserta para luego poder mostrarlo. insertedId recupera el id del item
         return item;
     }
 }
